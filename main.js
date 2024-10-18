4030cf3d367ce5b6a6434a4a7f3a65731c7d5747
mLascurain <matiaslascurain04@gmail.com>
2024-10-18 17:37:19 -0300

    added some basic functions

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
-import "/css/style.css";
+const myLibrary = [];
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Kiwbary</h1>
-    <p class="read-the-docs">
-      this is a sample text
-    </p>
-  </div>
-`;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+const userNumber = prompt("How many books do you want to load?");
+for (let i = 0; i < userNumber; i++) {
+  const userTitle = prompt("Please enter the title of the book: ");
+  const userAuthor = prompt("Please enter the author of the book: ");
+  const userPages = prompt("Please enter the pages of the book: ");
+  const userState = prompt("Have you read the book? ");
+
+  addBookToLibrary(userTitle, userAuthor, userPages, userState);
+}
+
+console.log(myLibrary);
+
+function displayBooks(myLibrary) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const bookDisplay = myLibrary[i];
+    const container = document.querySelector(".books-container");
+    if (!container) {
+      console.error(
+        "No container element found with the class 'books-container'",
+      );
+      return;
+    }
+    const book = document.createElement("div");
+    book.classList.add("book"); // Add class for styling (if needed)
+    book.innerHTML = `
+        <h2>${bookDisplay.title}</h2>
+        <h3>${bookDisplay.author}</h3>
+        <p>${bookDisplay.pages}</p>
+        <p>${bookDisplay.read}</p>
+    `;
+    container.appendChild(book);
+  }
+}
+
+// Make sure the function runs after the DOM is loaded
+window.onload = function () {
+  displayBooks(myLibrary);
+};
