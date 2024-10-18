6739dc457bc80eaefce1c19d22d29204bfa73c6d
mLascurain <matiaslascurain04@gmail.com>
2024-10-18 16:34:11 -0300

    first upload

---
+++ main.js
@@ -0,0 +1,10 @@
+import "/css/style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Kiwbary</h1>
+    <p class="read-the-docs">
+      this is a sample text
+    </p>
+  </div>
+`;
